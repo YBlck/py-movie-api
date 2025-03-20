6f6421597a76265e46a0651d392effc18b4bf2ba
Yura Chornyi <chorny.yura@gmail.com>
2025-03-21 01:59:58 +0200

    chore: change url name

--- cinema/urls.py
+++ cinema/urls.py
@@ -5,6 +5,6 @@ from cinema.views import movie_list, movie_detail
 app_name = "cinema"
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("movies/", movie_list, name="movie_list"),
+    path("movies/<int:pk>/", movie_detail, name="movie_detail"),
 ]
